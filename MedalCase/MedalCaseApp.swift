7803af4981394aca30baf995d0606fff770e8694
ismailgok33 <ismailgok33@gmail.com>
2026-07-18 19:14:24 -0400

    Initial Commit

---
+++ MedalCase/MedalCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedalCaseApp.swift
+//  MedalCase
+//
+//  Created by Ismail Gok on 2026-07-18.
+//
+
+import SwiftUI
+
+@main
+struct MedalCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
